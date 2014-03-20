b05815adea4ca5fad7ca7fd90eb83fb4aa4faf9f
Wanglei <nihiue@gmail.com>
2014-03-20 11:57:09 +0800

    added pm25in.py: fetch AQ data from pm2.5.in
    Signed-off-by: Wanglei <nihiue@gmail.com>

---
+++ AQI_pm2.5in/pm25in.py
@@ -0,0 +1,6 @@
+import urllib2
+import cookielib
+urllib2.install_opener(urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))) #Handle the cookie
+urllib2.urlopen("http://www.pm25.in/api/querys/aqi_details.json?city=shanghai&token=5j1znBVAsnSf5xQyNQyq") #Give me some cookie!
+response = urllib2.urlopen("http://www.pm25.in/api/querys/aqi_details.json?city=shanghai") #Second request with cookie
+print response.read()
