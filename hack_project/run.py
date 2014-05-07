f6f2c9198dcda645410cf51c1a83fa56def96b7b
Wanglei <nihiue@gmail.com>
2014-05-07 17:07:05 +0800

    finished login&logout
    Signed-off-by: Wanglei <nihiue@gmail.com>

--- hack_project/run.py
+++ hack_project/run.py
@@ -1,11 +1,19 @@
 #encoding: utf-8
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import g,Flask,request,render_template,session,abort,redirect, url_for
+from functools import wraps
 from db_support import DBconnection,email_validator
-from flask import abort, redirect, url_for
 from hashlib import md5
+
 app = Flask(__name__)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('logged_in') is not True:
+             return render_template('redir.html',message=u"需要登录...",tar_url=u"/login",tar_name=u"登录页面")
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 def index():
@@ -30,8 +38,10 @@ def about():
     return render_template('about.html');
 
 @app.route('/login')
-def login_page():
-	return render_template('login.html');  
+def login_page():    
+    if session.get('logged_in') is True:
+        return render_template('redir.html',message=u"已经登陆",tar_url=u"/user_center",tar_name=u"用户中心")
+    return render_template('login.html');  
 @app.route('/login/submit',methods=['POST'])
 def login_submit():
     try:
@@ -42,7 +52,12 @@ def login_submit():
         pass_md5=md5(email_add+request.form["password"]).hexdigest().upper()
         if(pass_md5!=query_res["Password"]):
             raise Exception("Password not match.")
-        return redirect(url_for('user_center'))
+        session['logged_in'] = True  
+        session["user"]={}     
+        session["user"]["id"]=str(query_res["_id"])
+        session["user"]["Email"]=query_res["Email"]        
+        session["user"]["Nickname"]=query_res["Nickname"]        
+        return redirect('/user_center')
 
     except Exception, e:
         return render_template('redir.html',message=u"登录失败:"+str(e),tar_url=u"/login",tar_name=u"登录页面")
@@ -50,6 +65,7 @@ def login_submit():
 @app.route('/reg')
 def reg_page():
     return render_template('reg.html');
+
 @app.route('/reg/checkemail',methods=['POST'])
 def reg_check():   
     try:
@@ -79,15 +95,26 @@ def reg_submit():
     except Exception, e:
         return render_template('redir.html',message=u"注册失败: "+str(e),tar_url=u"/reg",tar_name=u"注册页面")
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)  
+    session.pop('user',None)   
+    return render_template('redir.html',message=u"登出成功",tar_url=u"/login",tar_name=u"登录页面")
+
 @app.route('/user_center/')
+@login_required
 def user_center():   
     return render_template('user_center/frame.html');
 
 @app.route('/user_center/page/<pagename>')
-def user_center_page(pagename):
+@login_required
+def user_center_page(pagename):   
+    if(pagename=="account_settings"):    
+        g.user=DBconnection.UserModel.find_one({"Email":session['user']['Email']})      
     return render_template('user_center/page/'+pagename+'.html');
 
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key="8E9852FD04BA946D51DE36DFB08E1DB6"
     app.run(host='0.0.0.0')
