f292e8bf5555fea689b9a17af0f6b29082b52549
frankxu2004 <frankxu2004@gmail.com>
2014-07-12 13:51:01 +0800

    might still be some 403 Forbidden error...

--- AQI_aqicnorg/aqicnorg.py
+++ AQI_aqicnorg/aqicnorg.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib2,urllib,json,re,cookielib,time
 
+def getpage(link):
+    content=opener.open(link,timeout=100).read()
+    return content
+
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 stations = json.loads(open("stations.txt",'r').read())
@@ -8,8 +12,14 @@ vmap = ['pm25','co','pm10','o3','so2','no2']
 tmap = ["PM2_5","CO","PM10","O3","SO2","NO2"]
 stationsdata = {}
 for link in stations:
-    print link
-    content=opener.open(link,timeout=100).read()
+    
+    try:
+        print link
+        content = getpage(link)
+    except:
+        print 'failed to open 1st'
+        time.sleep(3)
+        content = getpage(link)
     soup = BeautifulSoup(content)
     citydivmain = soup.find('div',id = 'citydivmain')
     stationdata = {}
